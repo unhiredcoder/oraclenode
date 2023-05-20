a961ff0f7a7ef1a47ad49867f2dea845ff85eea4
unhiredcoder <proboys777333@gmail.com>
2023-05-20 21:43:02 +0530

    first

---
+++ index.js
@@ -0,0 +1,27 @@
+const oracledb = require('oracledb');
+(async function () {
+  
+    const connection = await oracledb.getConnection({
+      user: 'system',
+      password: 'aditya',
+      connectString: 'localhost/xe'
+    });
+
+    // const sql = `
+    //   INSERT INTO user905 (id,name, password, email, country)     
+    //   VALUES (1006,'niraj', 'abc23', 'joker@gmail.com', 'india')
+    // `;
+
+//     const sql = `
+//     UPDATE user905
+//     SET country = 'JAPAN'
+//     WHERE id = 1005
+//   `;
+
+    const data = await connection.execute(sql);
+    console.log(data.rowsAffected + " row(s) inserted");
+    await connection.commit();
+    const result = await connection.execute('SELECT * FROM user905');
+    console.log(result.rows);
+}
+)()

7c21551ba077d1347c86e0bbb49adb18a01f6dcf
unhiredcoder <proboys777333@gmail.com>
2023-05-21 17:08:27 +0530

    second

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ const oracledb = require('oracledb');
     //   VALUES (1006,'niraj', 'abc23', 'joker@gmail.com', 'india')
     // `;
 
-//     const sql = `
-//     UPDATE user905
-//     SET country = 'JAPAN'
-//     WHERE id = 1005
-//   `;
+  //   const sql = `
+  //   UPDATE user905
+  //   SET country = 'JAPAN'
+  //   WHERE id = 1005
+  // `;
 
     const data = await connection.execute(sql);
     console.log(data.rowsAffected + " row(s) inserted");
-    await connection.commit();
+    await connection.commit();  //if not commit ,then forget data what u added hahahaha
     const result = await connection.execute('SELECT * FROM user905');
     console.log(result.rows);
 }
